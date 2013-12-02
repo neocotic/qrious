baf4f9b3709ea23f082166070a15d841379ef489
neocotic <mercer.alasdair@gmail.com>
2013-12-02 13:27:53 +0000

    added grunt build system and improved developer documentation

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+
+  // Configuration
+  // -------------
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    docco: {
+      all: {
+        options: {
+          output: 'docs'
+        },
+        src: 'qr.js'
+      }
+    },
+
+    uglify: {
+      all: {
+        files: {
+          'qr.min.js': 'qr.js'
+        },
+        options: {
+          banner: (
+              '/*! <%= pkg.name %> v<%= pkg.version %> | (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> | <%= pkg.licenses[0].type %> License'
+            + '\njsqrencode | (c) 2010 tz@execpc.com | GPL v3 License'
+            + '\n*/ '
+          )
+        }
+      }
+    }
+
+  });
+
+  // Tasks
+  // -----
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-docco');
+
+  grunt.registerTask('dist', [ 'uglify', 'docco' ]);
+
+};
