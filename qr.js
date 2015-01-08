f506d99406f5edab3d2c7dfc8d2c2869a70b76cd
Richard Hoffman <coverslide@users.noreply.github.com>
2015-01-08 15:51:14 -0800

    Add process.browser check for inNode

--- qr.js
+++ qr.js
@@ -1188,17 +1188,20 @@
 
   // Export `qr` for node.js and CommonJS.
   if (typeof exports !== 'undefined') {
-    inNode = true;
+    // Detect if we are using CommonJS *without* Node i.e. browserify
+    inNode = !(typeof process !== 'undefined' && process.browser); // browserify-specific check
 
     if (typeof module !== 'undefined' && module.exports) {
       exports = module.exports = qr;
     }
     exports.qr = qr;
-
-    // Import required node.js modules.
-    Canvas = require('canvas');
-    Image = Canvas.Image;
-    fs = require('fs');
+    
+    if (inNode) {
+      // Import required node.js modules.
+      Canvas = require('canvas');
+      Image = Canvas.Image;
+      fs = require('fs');
+    }
   } else if (typeof define === 'function' && define.amd) {
     define(function () {
       return qr;
